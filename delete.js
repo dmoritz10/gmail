07b994aa869de361ab17f2d65e9883d69c4c286a
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-04-12 09:46:26 -0700

    dan

--- delete.js
+++ delete.js
@@ -1,7 +1,13 @@
 
 function btnGmailDeleteHtml() {
 
-    console.log('listGmailLabels', listGmailLabels())
+    var labels = listGmailLabels()
+
+    for (var i=0;i<labels.length;i++) {
+        $('#gmail-label-select').append('<option>'+labels[i]+'</option>')
+      }
+
+    console.log('listGmailLabels', labels)
 
     gotoTab('GmailDelete')
 
