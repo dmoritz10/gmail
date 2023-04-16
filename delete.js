975e34a500d5ce0e383e417c2bf6a7fa8910e046
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-04-16 14:21:21 -0700

    dan

--- delete.js
+++ delete.js
@@ -91,7 +91,7 @@ async function deleteGmails(shtTitle) {
 
   console.log('msgIdsArr',msgIdsCol,  msgIdsArr)
 
-  var batchSize = 1
+  var batchSize = 2
   var pntr = msgIdsArr.length-1
 
   console.log('pntr', pntr)
